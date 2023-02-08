fcd15b2ca061bcf8fdf1290a5e9ca9f16a67e375
hushicai <bluthcy@gmail.comm>
2023-02-08 17:24:45 +0800

    feat: 每次会话允许失败重试一次

--- src/chatgpt/index.js
+++ src/chatgpt/index.js
@@ -3,36 +3,51 @@ import dotenv from 'dotenv'
 
 const env = dotenv.config().parsed // 环境参数
 
+const delay = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(true), ms)
+  })
+}
+
 const api = new ChatGPTAPI({
   apiKey: env.OPENAI_API_KEY,
-  debug: false
+  debug: false,
 })
 
-const ConversationPool= {};
+const ConversationPool = {}
+const RetryPool = {}
 
 // 获取 chatGPT 的回复
 export async function getChatGPTReply(talkerId, content) {
-  console.log('/ talkerId', talkerId)
-  console.log('/ content', content)
-  const conversation = ConversationPool[talkerId];
-  // 调用ChatGPT的接口
-  const reply = await api.sendMessage(content, {
-    conversationId:  conversation?.conversationId,
-    parentMessageId: conversation?.messageId,
-    //  "ChatGPT 请求超时！最好开下全局代理。"
-    timeoutMs: 2 * 60 * 1000,
-  })
-  ConversationPool[talkerId] = {
-    messageId: reply.id,
-    conversationId: reply.conversationId
-  }
-  console.log('/ reply', reply.text)
-  return reply.text
+  RetryPool[talkerId] = 1
+  return getReply(talkerId, content)
+}
 
-  // // 如果你想要连续语境对话，可以使用下面的代码
-  // const conversation = api.getConversation();
-  // return await conversation.sendMessage(content, {
-  //   //  "ChatGPT 请求超时！最好开下全局代理。"
-  //   timeoutMs: 2 * 60 * 1000,
-  // });
+async function getReply(talkerId, content) {
+  try {
+    console.log('[content]: ', content)
+    const conversation = ConversationPool[talkerId]
+    // 调用ChatGPT的接口
+    const reply = await api.sendMessage(content, {
+      conversationId: conversation?.conversationId,
+      parentMessageId: conversation?.messageId,
+      //  "ChatGPT 请求超时！最好开下全局代理。"
+      timeoutMs: 2 * 60 * 1000,
+    })
+    ConversationPool[talkerId] = {
+      messageId: reply.id,
+      conversationId: reply.conversationId,
+    }
+    console.log('[reply]: ', reply.text)
+    return reply.text
+  } catch (e) {
+    if (RetryPool[talkerId]) {
+      delete RetryPool[talkerId]
+      console.log(e)
+      console.log('retry...')
+      await delay(1000)
+      return getReply(talkerId, content)
+    }
+    throw e
+  }
 }
